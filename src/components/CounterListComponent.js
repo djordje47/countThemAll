53225356b324b22fbaa8dbc31b763023e7e24fce
Djordje Arsenovic <dj.arsenovic@gmail.com>
2019-12-27 17:03:08 +0100

    Added list element of single counter.

--- src/components/CounterListComponent.js
+++ src/components/CounterListComponent.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function CounterListComponent(props) {
     console.log(props.counters);
-  return (
-    <div>
-        <ul>
-            
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                {props.counters.map((counter) => <li>{counter.counterName}</li>)}
+            </ul>
+        </div>
+    );
 }
